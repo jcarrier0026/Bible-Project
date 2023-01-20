436f0fdb5e2676d2bb0ed06af03aec89c2ae9129
Joshua Carrier <jcarrier@Joshuas-MacBook-Pro.local>
2023-01-19 19:15:02 -0500

    Organized Folders
    Had dup-licate folders and lots of nested directories for no reason.

---
+++ ReactApp/src/App.js
@@ -0,0 +1,12 @@
+import SearchBar from "./SearchBar"
+
+function App() {
+  return (
+	<div>
+		<h1>Bible App</h1>
+		<SearchBar />
+	</div>
+  );
+}
+
+export default App;
---
+++ ReactApp/src/SearchBar.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+	return (
+		<div>
+			<input type="search" id="bible_search" name="bible_search"></input>
+			<button>Search</button>
+		</div>
+		);
+}
+export default SearchBar;
