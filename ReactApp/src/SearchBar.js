7c1c3b5026ac3941dd57691668db1769dbf4b20a
Joshua Carrier <jcarrier@Joshuas-MacBook-Pro.local>
2023-01-31 02:52:12 -0500

    Enter Button.
    You can now use the enter button to activate the search.

--- ReactApp/src/SearchBar.js
+++ ReactApp/src/SearchBar.js
@@ -1,24 +1,25 @@
 import {useState} from "react";
-//import { verses } from "./text_files/text.txt";
 
 var verses = ["For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.",
 				"In the beginning, God created the heavens and the earth.",
 				"As iron sharpens iron, so one man sharpens another."];
 
+// This function takes the text from the search bar and searches for a match
 function InputHandler(text) {
 	var output = []
 	for(var verse in verses) {
-		if (verses[verse].search(new RegExp(text, "i")) !== -1) {
-			console.log("Verse found:");
-			console.log(verses[verse]);
+		if (verses[verse].search(new RegExp(text, "i")) !== -1 && text !== "") {
+			//console.log("Verse found:");
+			//console.log(verses[verse]);
 			output.push(verses[verse]);
 		}
 	}
 	SetVersesList(output);
 	//console.log("Searched for: " + text);
-	return output;
+	//return output;
 }
 
+// This function takes the verses that match the search criteria and display them on the webpage.
 function SetVersesList(verses) {
 	var element = document.getElementById("verses_display");
 	element.innerHTML = "";
@@ -27,12 +28,22 @@ function SetVersesList(verses) {
 	}
 }
 
+function CheckKey(e) {
+	var button = document.getElementById("search_button");
+	console.log(e.key);
+	if (e.key === "Enter") {
+		e.preventDefault();
+		button.click();
+	}
+}
+
+// This is the layout for the webpage;
 function SearchBar() {
 	const [search_term, SetSearchTerm] = useState("");
 	return (
 		<div>
-			<input type = "search" id = "bible_search" name = "bible_search" onChange = {event => {SetSearchTerm(event.target.value)}}></input>
-			<button value = {search_term} onClick = {e => InputHandler(e.target.value)}>Search</button>
+			<input type = "search" id = "search_bar" name = "bible_search" onChange = {event => {SetSearchTerm(event.target.value)}} onKeyDown = {e => CheckKey(e)}></input>
+			<button id = "search_button" value = {search_term} onClick = {e => InputHandler(e.target.value)}>Search</button>
 			<p id = "verses_display" ></p>
 		</div>
 		);
